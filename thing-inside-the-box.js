17ee907ce9e3b7743ca4c6dd643e91e5911c2b01
Seven <jaseveen@gmail.com>
2020-05-06 10:37:32 +0200

    add thing-inside-the-box.js

---
+++ thing-inside-the-box.js
@@ -0,0 +1,45 @@
+/*
+* Opanuj JavaScript - Przeprogramowani.pl
+* I. Fundamenty języka JavaScript
+*
+* Ćwiczenie 11 - Thing inside the box
+*/
+
+/*
+* Cel zadania
+*------------
+* Zaimplementuj funkcję, sprawdzającą czy pudełko jest puste.
+*
+*
+*/
+function thing(box) {
+    if (box.includes('o') && box.charAt(box.indexOf('o')-2) === '*' && box.charAt(box.indexOf('o')+2) === '*') {
+      return true
+    } return false
+  }
+  
+  
+  /* Weryfikacja */
+  
+  function verify(input, goal) {
+    if (input === goal) {
+      console.log('Gratulacje!');
+    } else {
+      console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+    }
+  }
+  
+  verify(thing(`*****
+                *   * o
+                *   *
+                *****`), false);
+  
+  verify(thing(`*****
+                * o *
+                *   *
+                *****`), true);
+  
+   verify(thing(`*****
+                 *   *
+                 *   *
+                 *****`), false);
