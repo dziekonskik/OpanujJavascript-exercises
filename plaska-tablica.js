56308ae9cfbcc8f7d25eceda543612dd503d7d39
Seven <jaseveen@gmail.com>
2020-05-15 18:32:19 +0200

    add plaska-tablica.js

---
+++ plaska-tablica.js
@@ -0,0 +1,37 @@
+/*
+* Cel zadania
+*------------
+* Weź tablicę zawierającą zagnieżdżone tablice i zwróć płaską tablicę zawierającą  wartości liczbowe bez powtórek oraz pozbawioną wartości null/undefined.
+*
+* Przykładowo: 
+* flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]) => [4, 3, 2, 1, 5]
+* 
+* Oczekuj tylko jednego poziomu zagnieżdżenia tablic.
+* 
+*/
+
+function flattenArray(deepArray) {
+    const toFlatten = deepArray.flat()
+    const numbersOnly = []
+    toFlatten.forEach(el => {
+      if (el !== null && el !== undefined) numbersOnly.push(el)
+    })
+    const noDuplicates = new Set(numbersOnly)
+    return Array.from(noDuplicates)
+  }
+  
+  
+  /* Weryfikacja */
+  
+  function verify(input, goal) {
+    input = Array.isArray(input) ? input.join(', ') : input;
+    if (input == goal) {
+      console.log('Gratulacje!');
+    } else {
+      console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+    }
+  }
+  
+  verify(flattenArray([4, [3, 2, undefined, 1], [1, 4, null, 5]]), "4, 3, 2, 1, 5");
+  verify(flattenArray([null, [1, 2, 3], [null, undefined]]), "1, 2, 3");
+  
