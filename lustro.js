ad99c59345d55599c4150e3e37e12c994e403b27
Seven <jaseveen@gmail.com>
2020-05-20 17:53:36 +0200

    add lustro.js

---
+++ lustro.js
@@ -0,0 +1,24 @@
+// # Przeprogramowani.pl - Opanuj JavaScript
+
+// Moduł II. - Przeglądarka bez tajemnic
+
+// ## Ćwiczenie nr. 1 - Lustro
+
+// ### Jak zacząć
+
+// Kliknij przycisk "Fork" u góry edytora aby skopiować ćwiczenie.
+
+// ### Cel ćwiczenia
+
+// Spraw, aby tekst lustrzany pojawiał się... w lustrze ;)
+
+// Dostęp do pola tekstowego uzyskasz poprzez klasę `.text-input`.
+// Dostęp do lustra uzyskasz poprzez klasę `.mirror`.
+
+const input = document.querySelector('.text-input');
+
+input.addEventListener('keyup', () => {
+  const mirror = document.querySelector('.mirror');
+  const reversed = [...input.value].reverse().join('');
+  mirror.innerHTML = reversed;
+});
