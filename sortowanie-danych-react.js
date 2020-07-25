25c28bf601d9f9ea11c94ef5750b847a624b434e
Seven <jaseveen@gmail.com>
2020-07-25 15:45:32 +0200

    add sortowanie-danych-react.js

---
+++ sortowanie-danych-react.js
@@ -0,0 +1,37 @@
+// # Przeprogramowani.pl - Opanuj JavaScript
+
+// Moduł III. - W świecie frameworków
+
+// ## Ćwiczenie nr. 20 - Sortowanie danych w React
+
+// ### Jak zacząć
+
+// Kliknij przycisk "Fork" u góry edytora aby skopiować ćwiczenie.
+
+// ### Warunek wstępny
+
+// Przejdź do tego ćwiczenia, dopiero po ukończeniu Sortowanie danych (Moduł II, ćwiczenie 8) w czystym JS.
+
+// ### Cel ćwiczenia
+
+// Wykonaj ponownie poniższe zadanie, tym razem w frameworku React. Nie podglądaj kodu, który napisałeś w  czystym JS. Pisząc wszystko od zera więcej się nauczysz :). Następnie wyciągnij wnioski odpowiadając pisemnie na załączone pytania.
+
+// ---
+
+// W tabeli znajdują się informacje o modułach kursu "Opanuj JavaScript".
+
+// Spraw aby:
+// 1. po kliknięciu na "Czas trwania" moduły sortowały się według czasu trwania
+// 2. trzałka przy napisie "Czas trwania" sugerowała użytkownikowi kolejność
+// 3. sekundy były wyświetlane w formacie hh:mm:ss
+// 4. Zaimplementuj animację zamiany kolejności wykorzystując animacje CSS.
+// 5. Odpowiedz na pytania z listy
+
+// ### Lista pytań
+
+// 1. Co było łatwiejsze do zaimplementowania w frameworku niż w czystym JS?
+// 2. Co było trudniejsze do zaimplementowania w frameworku niż w czystym JS?
+// 3. Które z rozwiązań ma mniej złożony kod? Porównując weź pod uwagę: prostotę, czytelność i objętość kodu.
+// 4. Czy uważasz, że użycie frameworka do rozwiązania tego zadania było uzasadniane? Dokonując oceny weź pod uwagę: czas realizacji, modularność i złożoność kodu.
+
+// https://stackblitz.com/edit/sortowanie-danych-w-react-wn1e8r?file=index.js  !!!!!!!    <<=== Live Preview
