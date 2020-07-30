203a7d3651de3f12c23b9ddb6f1ac491a5443433
Seven <jaseveen@gmail.com>
2020-07-30 16:00:34 +0200

    add pagination-REACT.js

---
+++ paginacja-REACT.js
@@ -0,0 +1,39 @@
+// # Przeprogramowani.pl - Opanuj JavaScript
+
+// Moduł II. - W świecie frameworków
+
+// ## Ćwiczenie nr. 19 - Paginacja w React
+
+// ### Jak zacząć
+
+// Kliknij przycisk "Fork" u góry edytora aby skopiować ćwiczenie.
+
+// ### Warunek wstępny
+
+// Przejdź do tego ćwiczenia, dopiero po wykonaniu Paginacja (Moduł II, ćwiczenie 14) w czystym JS.
+
+// ### Cel ćwiczenia
+
+// Wykonaj ponownie poniższe zadanie, tym razem w frameworku React. Nie podglądaj do kodu, który napisałeś w  czystym JS. Pisząc wszystko od zera więcej się nauczysz :). Następnie wyciągnij wnioski odpowiadając pisemnie na załączone pytania.
+
+// Wyświetlanie dużej ilości danych bywa uciążliwe. Paginacja (stronicowanie) to sposób aby sobie z tym poradzić.
+
+// Zaimplementuj prosty mechanizm pagiancji, który na jednej stronie wyświetli 2 elementy z listy.
+// 1. Przyciski "Previous" & "Next" powinny się wyłączać w chwili gdy jesteśmy odpowiednio na pierwszej i ostatniej stronie paginacji.
+// 2. Odpowiedz na pytania z listy
+
+// ### Lista pytań
+
+// 1. Co było łatwiejsze do zaimplementowania w frameworku niż w czystym JS?
+// 2. Co było trudniejsze do zaimplementowania w frameworku niż w czystym JS?
+// 3. Które z rozwiązań ma mniej złożony kod? Porównując weź pod uwagę: prostotę, czytelność i objętość kodu.
+// 4. Czy uważasz, że użycie frameworka do rozwiązania tego zadania było uzasadniane? Dokonując oceny weź pod uwagę: czas realizacji, modularność i złożoność kodu.
+
+// this one was problematic but i have learned a lot about passing data from children to parent and using lifecycle functions
+//
+//
+//
+//
+//
+//
+// https://stackblitz.com/edit/paginacja-w-react-fjfdsa?file=Pagination.js    !!!!!!!    <<=== Live Preview
